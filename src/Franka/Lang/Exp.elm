894de453c17802d2c24eeab888b562cbc0416d0a
ozmi <attila.mihaly.jr@gmail.com>
2018-02-24 18:26:49 -0500

    Added all exp types

--- src/Franka/Lang/Exp.elm
+++ src/Franka/Lang/Exp.elm
@@ -2,6 +2,16 @@ module Franka.Lang.Exp exposing (..)
 
 import Franka.Name
 
+type Exp a
+  = Const a
+  | Ident Name
+  | Apply (Exp a) (Exp a)
+  | Lambda Name (Exp a)
+  | Let Name (Exp a) (Exp a)
+  | Select (Exp a) (Selector a)
+  | Branch (Exp a) (List ((Selector a), (Exp a)))
+  
+
 type Selector a
   = SelectIndex Int
   | SelectName Name
